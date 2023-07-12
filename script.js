ba445b88559f27853048c88b9c4871c6c06fd940
newprice247 <newprice247@gmail.com>
2023-07-12 08:14:20 -0400

    add letters, numbers, and characters, and randomized variables

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+let chars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+let randomA = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+let randomN = numbers[Math.floor(Math.random() * numbers.length)];
+
+let randomC = chars[Math.floor(Math.random() * chars.length)];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
